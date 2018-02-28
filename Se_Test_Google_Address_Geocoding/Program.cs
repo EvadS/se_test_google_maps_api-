20c525b9da301632ce0451fc8ea47162125f2e4e
Evgeniy.Skiba <Skiba.Evgeniy@hotmail.com>
2018-02-28 15:41:55 +0200

    Add project files.

---
+++ Se_Test_Google_Address_Geocoding/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Se_Test_Google_Address_Geocoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
